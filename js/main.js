ef3f9e6804c852a65dd046ec14fdeffe18c65b29
Дима <sultanovdu@gmail.com>
2021-06-08 12:14:21 +0300

    Исправил ошибки

--- js/main.js
+++ js/main.js
@@ -1,17 +1,17 @@
 function getRndInteger(min, max) {
   if (max > min) {
-  return Math.floor(Math.random() * (max - min + 1) ) + min;
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
   } else {
-  return getRndInteger = 0
+    return getRndInteger = 0;
   }
 }
 getRndInteger(0, 25);
 
 function getRndFloat(min, max) {
   if (max > min) {
-   return Math.random() * (max - min);
+    return Math.random() * (max - min);
   } else {
-  return Math.random() * (min - max);
+    return Math.random() * (min - max);
   }
 }
 getRndFloat (0, 1).toFixed(1);
